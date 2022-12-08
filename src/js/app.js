e2dd45742cfd4a4403bb0957768d14f41d35bf23
petrborr <petrborr@gmail.com>
2022-12-08 11:23:11 +0200

    add: data-price attribute to product

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,11 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const products = document.getElementsByClassName("product")
+  const price = document.getElementsByClassName("price")[0].textContent
+  for (let i = 0; i < products.length; i++) {
+    products[i].setAttribute("data-price", price)
+  }
 
 
 });
